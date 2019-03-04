9a1bbe0ce794f2e16b1bcf063af59ece56531170
Maximilian Groß <maximilian@elektro-gross.at>
2019-03-04 17:40:28 +0100

    [TASK] UploadController + Flashcardscontroller

--- app/Http/Controllers/FlashcardsController.php
+++ app/Http/Controllers/FlashcardsController.php
@@ -3,5 +3,22 @@
  * Created by PhpStorm.
  * User: Max-Game
  * Date: 04.03.2019
- * Time: 17:31
+ * Time: 17:32
  */
+
+namespace App\Http\Controllers;
+
+class FlashcardsController extends Controller {
+    public function alleFlashcards() {
+
+    }
+    public function neueFlashcard() {
+
+    }
+    public function loescheFlashcard($id) {
+
+    }
+    public function updateFlashcard($id) {
+
+    }
+}
--- app/Http/Controllers/Uploadcontroller.php
+++ app/Http/Controllers/Uploadcontroller.php
@@ -5,3 +5,11 @@
  * Date: 04.03.2019
  * Time: 17:32
  */
+
+namespace App\Http\Controllers;
+
+class Uploadcontroller extends Controller {
+    public function upload() {
+
+    }
+}
