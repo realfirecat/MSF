158cb5b208a5cde691fbd3476d2aa5314292a5e3
Siolet <Jessie2409@hotmail.de>
2019-04-29 17:31:00 +0200

    [TASK] Visualisierung von flashcardsAnsehen fertiggestellt

--- public/js/flashcardsAnsehen.js
+++ public/js/flashcardsAnsehen.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $.ajax({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
@@ -11,7 +10,31 @@ $(document).ready(function() {
         contentType: false,
     })
         .done(function (json) {
+            let container = document.querySelector('#collapseOne .row');
+            console.log(container);
             for (let item of json.data) {
+                container.innerHTML += "  <div class=\"col-md-3 flashcard\">\n" +
+                    "                        <div class=\"flashcard-text mb-3 text-center\" data-toggle=\"modal\" data-target=\"#fc" + item.id + "_content\">\n" +
+                    item.title +
+                    "                        </div>\n"  +
+                    " <div id=\"fc" + item.id + "_content\" class=\"modal fade bd-modal-lg\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"myLargeModalLabel\" aria-hidden=\"true\">\n" +
+                    "                            <div class=\"modal-dialog modal-lg modal-dialog-centered\">\n" +
+                    "                                <div class=\"modal-content\">\n" +
+                    "                                    <div class=\"modal-header\">\n" +
+                    "                                        <h5 class=\"modal-title\" id=\"ModalLongTitle\">Kategorie 1 - " + item.title + item.id + "</h5>\n" +
+                    "                                        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n" +
+                    "                                            <span aria-hidden=\"true\">&times;</span>\n" +
+                    "                                        </button>\n" +
+                    "                                    </div>\n" +
+                    "                                    <div class=\"modal-body\">\n" +
+                    "                                        <p>" + item.content + "</p>" +
+                    "                                    </div>\n" +
+                    "                                </div>\n" +
+                    "                            </div>\n" +
+                    "                        </div>" +
+                    "                    </div>";
+
+
                 console.log(item.id, item.title, item.content);
             }
         })
@@ -20,5 +43,4 @@ $(document).ready(function() {
         });
 
     console.log("gesendet");
-
 });
--- resources/views/flashcardsAnsehen.blade.php
+++ resources/views/flashcardsAnsehen.blade.php
@@ -7,13 +7,168 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
           integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <!--script src="js/bootstrap.js"></script-->
+    {{--<script src="../../resources/js/bootstrap.js"></script>--}}
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>MSF</title>
     <script
-            src="../../node_modules/jquery/dist/jquery.js"></script>
+            src="http://code.jquery.com/jquery-3.3.1.min.js"
+            integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+            crossorigin="anonymous"></script>
     <script src="{{ asset('js/flashcardsAnsehen.js') }}"></script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body>
+
+<div class="navigation">
+    <div class="nav-left mt-4">
+        <a>MITSCHRIFT FLASHCARDS</a>
+    </div>
+    <div class="nav-right mt-4">
+        <a class="button mr-3" href="#section4">CONVERT NOW</a>
+        <a class="mr-2">Username</a>
+    </div>
+</div>
+
+<div id="section1" class="container flashcards-page">
+    <h1 class="text-center">Flashcards</h1>
+    <div class="searchbar form-group has-search">
+        <input type="text" class="form-control" placeholder="Kategorie suchen...">
+    </div>
+    <div class="accordion" id="accordion">
+        <div class="card">
+            <div class="kategorie card-header" id="headingOne">
+                <h5 class="mb-0">
+                    <button class="btn btn-link" id="kat1" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Kategorie 1
+                    </button>
+                </h5>
+            </div>
+
+            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="row p-5">
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingTwo">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        Kategorie 2
+                    </button>
+                </h5>
+            </div>
+            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="row p-5">
+                    <div class="col-md-3 flashcard">
+                        <div id="fc2" class="flashcard-text text-center" data-toggle="modal" data-target="#fc2_content">
+                            Flashcard 1
+                        </div>
+                        <div id="fc2_content" class="modal fade bd-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="ModalLongTitle2">Kategorie 2 - Flashcard 1</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 2
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 3
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 4
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header" id="headingThree">
+                <h5 class="mb-0">
+                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        Kategorie 3
+                    </button>
+                </h5>
+            </div>
+            <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div class="row p-5">
+                    <div class="col-md-3 flashcard">
+                        <div id="fc3" class="flashcard-text text-center" data-toggle="modal" data-target="#fc3_content">
+                            Flashcard 1
+                        </div>
+                        <div id="fc3_content" class="modal fade bd-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg modal-dialog-centered">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="ModalLongTitle3">Kategorie 3 - Flashcard 1</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+
+                                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+                                            At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 2
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 3
+                        </div>
+                    </div>
+                    <div class="col-md-3 flashcard">
+                        <div class="flashcard-text text-center">
+                            Flashcard 4
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="align-bottom">
+    <div class="text-center p-3 footer-text">
+        <a>© 2018 Copyright: Mitschrift Flashcards</a>
+    </div>
+</footer>
+
 </body>
 </html>
