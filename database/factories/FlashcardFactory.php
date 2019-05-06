ee2e221fe5cb6baabcc73fb5dadd932261d5e905
TobitoZz <daniel.schuch@htl.rennweg.at>
2019-05-06 21:51:17 +0200

    Überarbeitung in database, hinzufügen des Tables 'isFavorite'

--- database/factories/FlashcardFactory.php
+++ database/factories/FlashcardFactory.php
@@ -10,6 +10,7 @@ $factory->define(Flashcard::class, function (Faker $faker) {
     return [
         'title' => Str::random(10),
         'content' => Str::random(100),
+        'isFavorite' => $faker->boolean,
         'fk_userID' => factory(User::class)->create()->id,
         'fk_themaID' => factory(Thema::class)->create()->id
     ];
