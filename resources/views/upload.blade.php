2a88265e1b1e890547350444c5f5ff73e42ddc7c
Maximilian Groß <maximilian@elektro-gross.at>
2019-05-13 16:20:46 +0200

    [BUGFIX] Fixed jquery url

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -11,7 +11,7 @@
     <!--script src="js/bootstrap.js"></script-->
     <title>MSF</title>
     <script
-            src="http://code.jquery.com/jquery-3.3.1.min.js"
+            src="https://code.jquery.com/jquery-3.3.1.min.js"
             integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
             crossorigin="anonymous"></script>
     <script src="{{ asset('js/script.js') }}"></script>
